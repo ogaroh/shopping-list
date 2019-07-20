425ae8799dc6d5304f0cd853c56cf5a6867201ae
ogaroh <ogaroerick1998@gmail.com>
2019-07-20 16:26:36 +0300

    add header + footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,14 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
