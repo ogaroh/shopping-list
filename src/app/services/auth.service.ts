16f9ecba089d373371619b1d06ee1a4364de958d
ogaroh <ogaroerick1998@gmail.com>
2019-07-21 11:37:57 +0300

    user model

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { AngularFirestore, AngularFirestoreDocument } from "@angular/fire/firestore";
+
+import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
